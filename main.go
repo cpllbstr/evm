744ee5c02b67e3d694690be661133d54789d25c0
Golubitskiy Savva <golubitskiy_savv@mail.ru>
2019-04-23 02:56:26 +0300

    Basic divide works, iterations in progress

--- main.go
+++ main.go
@@ -14,22 +14,23 @@ type graph struct {
 }
 
 type group struct {
-	cand   []int //кандидаты в группе
-	cap    int   //максимальное число кандидатов в группе
-	filled bool
+	verts     []int //кандидаты в группе
+	cap       int   //максимальное число кандидатов в группе
+	outdeltas []int
+	filled    bool
 }
 
 func CreateGroups(n []int) []group {
 	grs := []group{}
 	for _, v := range n {
-		grs = append(grs, group{cap: v, cand: []int{}, filled: false})
+		grs = append(grs, group{cap: v, verts: []int{}, filled: false})
 	}
 	return grs
 }
 
 func (gp *group) Add(vert int) {
-	if len(gp.cand) < gp.cap {
-		gp.cand = append(gp.cand, vert)
+	if len(gp.verts) < gp.cap {
+		gp.verts = append(gp.verts, vert)
 	} else {
 		panic("Adding more then capacity")
 	}
@@ -37,7 +38,7 @@ func (gp *group) Add(vert int) {
 }
 
 func (gp *group) CheckFilled() bool {
-	if gp.cap == len(gp.cand) {
+	if gp.cap == len(gp.verts) {
 		gp.filled = true
 	}
 	return gp.filled
@@ -52,56 +53,26 @@ func NewGraph(matr [][]int) graph {
 	return g
 }
 
-//summArr - суммирует числа в массиве
-func summArr(v []int) int {
-	p := 0
-	for _, val := range v {
-		p += val
-	}
-	return p
-}
-
 //Plot -  выводит текущее состояние матрицы смежности, вроде как красивенько даже
-func (g graph) Plot() {
+func (g graph) PlotSubm(subm []int) {
 	fmt.Println()
 	fmt.Print("  |")
-	for _, v := range g.config {
-		fmt.Printf("%3v", v)
+	for _, v := range subm {
+		fmt.Printf("% 3v", v)
 	}
 	fmt.Println()
-	for range g.config {
+	for range subm {
 		fmt.Print("---")
 	}
 	fmt.Println("---")
-	for _, i := range g.config {
+	for _, i := range subm {
 		fmt.Print(i, " |")
-		for _, j := range g.config {
-			fmt.Printf("%3v", g.distanses[i][j])
+		for _, j := range subm {
+			fmt.Printf("% 3v", g.distanses[i][j])
 		}
 		fmt.Println()
 	}
 	fmt.Println()
-}
-
-/*
-func (g graph) PrintMat() {
-	for i, strt := range g.distanses {
-		fmt.Printf("%v|  %v -> %v\n", g.config[i], strt, g.Power(i))
-	}
-}
-*/
-
-//PowerGroup - считает степени вершин
-/*
-	strt - индекс начального элемента диагональной подматрицы mat[strt][strt]
-	finl - индекс конечного элемента
-*/
-func (g graph) PowerSubMatrix(subm []int) []int {
-	var res []int
-	for i := range subm {
-		res = append(res, g.Power(i, subm))
-	}
-	return res
 }
 
 //Power - степень вершины в подматрице
@@ -143,15 +114,15 @@ func ConfigWithout(conf, verts []int) []int {
 	return confw
 }
 
-// Neighbors - возвращает смежные c подмножеством вершины
-func (g graph) Neighbors(verts []int) []int {
+// Neighbors - возвращает смежные c подмножеством вершины, ищет вершины по всему графу
+func (g graph) Neighbors(verts, subm []int) []int {
 	neigh := []int{}
-	for _, vert := range verts {
-		for vertex, distanse := range g.distanses[vert] {
-			if distanse > 0 {
-				fl1, fl2 := CheckExist([]int{vertex}, neigh), CheckExist([]int{vertex}, verts)
+	for _, vert := range verts { //вершины для которых ищем соседей
+		for _, submvert := range subm { //в каком подмножестве
+			if g.distanses[vert][submvert] > 0 {
+				fl1, fl2 := CheckExist([]int{submvert}, neigh), CheckExist([]int{submvert}, verts)
 				if fl1 && fl2 {
-					neigh = append(neigh, vertex)
+					neigh = append(neigh, submvert)
 				}
 			}
 		}
@@ -177,7 +148,7 @@ func (g graph) FindSubMins(subm []int) []int {
 			min = pow
 		}
 	}
-	for i := range g.distanses {
+	for _, i := range subm {
 		pow := g.Power(i, subm)
 		if min == pow {
 			minind = append(minind, i)
@@ -196,60 +167,132 @@ func (g graph) BetterCands(cand, subm []int) ([]int, int) {
 		deltas = append(deltas, g.Power(c, subm)-g.Power(c, cand))
 	}
 	max := -10000
+	min := 10000
+	mini := 0
 	maxi := 0
 	for i, delta := range deltas {
 		if delta < max {
 			max = delta
 			maxi = i
 		}
+		if delta > min {
+			min = delta
+			mini = i
+		}
 	}
-	fmt.Println("B:", cand, subm, cand[maxi])
-	return ConfigWithout(cand, []int{cand[maxi]}), cand[maxi]
+	return ConfigWithout(cand, []int{cand[maxi]}), cand[mini]
+
+}
+
+func (g graph) CountNeigh(verts, subm []int) ([]int, int) {
+	neigh := make([]int, len(verts))
+	for i, vert := range verts { //вершины для которых ищем соседей
+		for _, submvert := range subm { //в каком подмножестве
+			if g.distanses[vert][submvert] > 0 {
+				fl1 := CheckExist([]int{submvert}, verts)
+				if fl1 {
+					neigh[i]++
+				}
+			}
+		}
+	}
+	min := 1000
+	mini := 0
+	for n, c := range neigh {
+		if min > c {
+			min = c
+			mini = n
+		}
+	}
+	return neigh, mini
+
+}
+
+func (gp *group) CalcDeltas(g graph) {
+	outer := ConfigWithout(g.config, gp.verts)
+	for _, v := range gp.verts {
+		gp.outdeltas = append(gp.outdeltas, g.Power(v, outer)-g.Power(v, gp.verts))
+	}
+}
+
+func (g graph) CreatePerM(grp []group) {
+	minlen := 1000
+	mini := 0
+	for i := range grp {
+		if minlen > len(grp[i].verts) {
+			mini = i
+		}
+	}
+	max := -1000
+	maxx := 0
+	maxy := 0
+	for i := 0; i < len(grp); i++ {
+		if i != mini {
+			for x := range grp[mini].outdeltas {
+				for y := range grp[i].outdeltas {
+					Pi := grp[mini].outdeltas[x] + grp[i].outdeltas[y] - 2*g.Power(grp[mini].verts[x], []int{grp[mini].verts[y]})
+					fmt.Print(" |", Pi)
+					if Pi > max {
+						maxx = x
+						maxy = y
+					}
+				}
+				fmt.Println()
+			}
+
+		}
+
+	}
+	fmt.Println(maxx, maxy)
 
 }
 
 func main() {
-	//ngroups := []int{5, 5, 5, 5, 5, 5}
-	//ngroups := []int{6, 6, 6, 6, 6}
-	//ngroups := []int{7, 7, 5, 5, 6}
-	ngroups := []int{4, 4}
-	g := NewGraph(test2)
-	g.Plot()
-	fmt.Println(g.FindSubMins([]int{0, 1, 2, 3, 4, 5, 6}))
+	divides := [][]int{{5, 5, 5, 5, 5, 5}, {6, 6, 6, 6, 6}, {7, 7, 5, 5, 6}}
+	ngroups := divides[2]
+
+	g := NewGraph(quest)
+	//g.PlotSubm(g.config)
 	grps := CreateGroups(ngroups)
 	currconf := g.config
-	for grupi := 0; grupi < len(grps); {
-		verts := g.FindSubMins(currconf)
-		fmt.Println("verts:", verts, currconf)
-		vertc := verts[0]
-		neigh := g.Neighbors([]int{vertc})
-		if len(neigh) == grps[grupi].cap {
-			for _, n := range neigh {
-				grps[grupi].Add(n)
-			}
-			grps[grupi].Add(vertc)
-		}
-		if len(neigh) > grps[grupi].cap {
-			cand := append(neigh, vertc)
-			for len(cand) != grps[grupi].cap {
-				cand, _ = g.BetterCands(cand, currconf)
-			}
-			grps[grupi].cand = cand
-		}
-		/*if len(neigh) < grps[grupi].cap {
-			fmt.Println(neigh)
-			cand := append(neigh, vertc)
-			for len(cand) != grps[grupi].cap {
-				cand = g.BetterCands(cand, currconf)
-			}
-			grps[grupi].cand = cand
-		}*/
-		fmt.Println("Cand:", grps[grupi].cand)
-		if grps[grupi].CheckFilled() {
-			currconf = ConfigWithout(currconf, grps[grupi].cand)
-			grupi++
-		}
-	}
-	fmt.Println(grps)
 
+	//нахождение опорного графа
+	for grupi := 0; grupi < len(grps); grupi++ {
+		verts := g.FindSubMins(currconf)
+		_, n := g.CountNeigh(verts, currconf)
+		vertc := verts[n]
+		cand := []int{vertc}
+		for !grps[grupi].filled {
+			if len(currconf) == grps[grupi].cap {
+				grps[grupi].verts = currconf
+				if grps[grupi].CheckFilled() {
+					break
+				}
+			} else {
+				for len(cand) < grps[grupi].cap {
+					neigh := g.Neighbors(cand, currconf)
+					_, bet := g.BetterCands(neigh, currconf)
+					cand = append(cand, bet)
+				}
+				grps[grupi].verts = cand
+				grps[grupi].CheckFilled()
+			}
+		}
+		currconf = ConfigWithout(currconf, grps[grupi].verts)
+	}
+
+	//fmt.Println(grps)
+
+	testmat := NewGraph(test2)
+	tgrps := []group{
+		group{cap: 4, verts: []int{0, 1, 2, 3}, outdeltas: []int{}, filled: true},
+		group{cap: 4, verts: []int{4, 5, 6, 7}, outdeltas: []int{}, filled: true},
+	}
+	for i := range tgrps {
+		tgrps[i].CalcDeltas(testmat)
+	}
+
+	testmat.CreatePerM(tgrps)
+
+	fmt.Println(tgrps)
 }
